9e33ea88f60e70f91971b860084012ba24a5df08
Vivek Gandharkar <vivekgandharkar@outlook.com>
2024-03-05 21:41:43 +0530

    complete exe 004

--- 004/exercise004.go
+++ 004/exercise004.go
@@ -1,21 +1,44 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strconv"
 	"strings"
 )
 
 // Ex004 takes a string of comma-separated numbers and returns a slice of int
 func Ex004(input string) []int {
-	// create a map with the size of n
-	numbers := strings.Split(input, ",")
 
-	length := len(numbers)
-	var num = make([]int, length)
+	strNums := strings.Split(input, ",")
 
-	for index, v := range numbers {
-		s := strings.Trim(v, " ")
-		num[index], _ = strconv.Atoi(s)
+	nums := []int{}
+
+	for _, strNum := range strNums {
+
+		num, _ := strconv.Atoi(strings.Trim(strNum, " "))
+
+		nums = append(nums, num)
+
 	}
-	return num
+
+	return nums
+
+}
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter comma separated numbers: ")
+
+	commaSeparatedNumsInput, _ := reader.ReadString('\n')
+
+	commaSeparatedNums := strings.TrimSuffix(commaSeparatedNumsInput, "\n")
+
+	nums := Ex004(commaSeparatedNums)
+
+	fmt.Println(nums)
+
 }
