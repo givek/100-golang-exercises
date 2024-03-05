099e3d012547818f3fe6ce8ecb8f5b6ee04947d9
Vivek Gandharkar <vivekgandharkar@outlook.com>
2024-03-05 21:53:35 +0530

    complete exe 006

--- 006/exercise006.go
+++ 006/exercise006.go
@@ -8,16 +8,19 @@ const h = 30
 // Ex006 calculates Q based on C=50 and H=50
 // Formula used: Q = Square root of [(2 * C * D)/H]
 func Ex006(d []int) []int {
-	var s = make([]int, len(d))
 
-	for i, num := range d {
-		a := (2 * c * num) / h
-		q := math.Sqrt(float64(a))
+	res := []int{}
 
-		var b int = int(math.Round(q))
+	for _, x := range d {
 
-		s[i] = b
+		r := float64((2 * c * x) / h)
+
+		sqrtOfR := math.Sqrt(r)
+
+		res = append(res, int(sqrtOfR))
+
 	}
 
-	return s
+	return res
+
 }
